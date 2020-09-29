6de55b90968e3616fc77a063fccd4f95d79e4eeb
Zachary Durr <zdurr22@gmail.com>
2020-09-29 19:49:44 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
-speed > 60 ? true : false
+speed >= 60 ? true : false
 speed < 40 ? true : false
 end
 
 
 
 def not_safe?(speed)
-speed > 60 ? true : false
-speed < 40 ? true : false 
+speed >= 60 ? true : false
+speed < 40 ? true : false
 end
